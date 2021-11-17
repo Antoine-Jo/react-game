7d21e82710bb1e70af03258de877d8519d2fc3ca
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2021-11-17 17:12:35 +0100

    Fix bug winner

--- src/index.js
+++ src/index.js
@@ -21,6 +21,9 @@ function Square(props) {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -41,7 +44,7 @@ function Square(props) {
       const winner = calculateWinner(this.state.squares);
       let status;
       if (winner) {
-          status = winner + 'a gagné';
+          status = 'Winner : ' + winner;
       } else {
           status = 'Prochain joueur : ' + (this.state.xIsNext ? 'X' : 'O');
       }
@@ -85,25 +88,6 @@ function Square(props) {
     }
   }
 
-  function calculateWinner(squares) {
-      const lines = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6],
-      ];
-      for (let i = 0; i < lines.length; i++) {
-          const [a, b, c] = lines[i];
-          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-              return squares[a];
-          }
-      }
-      return null;
-  }
   
   // ========================================
   
@@ -111,4 +95,24 @@ function Square(props) {
     <Game />,
     document.getElementById('root')
   );
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   
